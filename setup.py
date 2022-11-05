bdec2cc7bb80516eb42c147ea5f8f529b9e8d743
Erik Call Reynolds <8844470+redlightningcomputingsolutions@users.noreply.github.com>
2022-11-05 13:02:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,15 @@ class build_ext(_build_ext):
     def finalize_options(self):
         _build_ext.finalize_options(self)
         # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
+         try:
+            __builtins__.__NUMPY_SETUP__ = False
+         except:
+            try:
+                # For python 3
+                import builtins
+                builtins.__NUMPY_SETUP__ = False
+            except:
+                print("Skipping numpy hack; if installation fails, try installing numpy first")
         import numpy
         self.include_dirs.append(numpy.get_include())
 
